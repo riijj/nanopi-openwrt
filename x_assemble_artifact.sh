e96cace29239b24d3490a6afb2daa8975e7fd4de
admin <admin@LOCALHOST>
2020-12-02 22:39:18 +0800

    add package release

--- x_assemble_artifact.sh
+++ x_assemble_artifact.sh
@@ -1,8 +1,13 @@
 gzip friendlywrt-*/out/*.img
-rm -rf ./artifact/ && mkdir -p ./artifact/
-mv friendlywrt-*/out/*img* ./artifact/
-cp friendlywrt-*/friendlywrt/.config ./artifact/
-cd ./artifact/
-md5sum *img* > md5sum.txt
-cd ..
-zip -r artifact.zip ./artifact/
+rm -rf ./artifact/ && mkdir -p ./artifact/rom && mkdir -p ./artifact/packages/
+mv friendlywrt-*/out/*img* ./artifact/rom/
+cp friendlywrt-*/friendlywrt/.config ./artifact/rom/
+mv friendlywrt-*/friendlywrt/staging_dir/packages/ ./artifact/packages/
+zip -r artifact.zip ./artifact/rom/
+zip -r artifact-packages.zip ./artifact/packages/
+
+#cp friendlywrt-*/friendlywrt/.config ./artifact/
+#cd ./artifact/
+#md5sum *img* > md5sum.txt
+#cd ..
+#zip -r artifact.zip ./artifact/
